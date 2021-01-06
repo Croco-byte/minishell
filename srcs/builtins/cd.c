1cc9bb16dc62a43f6e43647114d4a0cd4dfcf409
qroland <quentin.roland@sciencespo.fr>
2021-01-06 15:59:51 +0100

    Added cd builtin. V1.0

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -1,21 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin.c                                          :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/06 12:35:25 by user42            #+#    #+#             */
-/*   Updated: 2021/01/06 15:57:31 by user42           ###   ########.fr       */
+/*   Created: 2021/01/06 15:50:05 by user42            #+#    #+#             */
+/*   Updated: 2021/01/06 15:56:14 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exec_builtin(char **args)
+int	ft_cd(char **args)
 {
-	if (ft_strcmp(args[0], "pwd") == 0)
-		ft_pwd(args);
-	if (ft_strcmp(args[0], "cd") == 0)
-		ft_cd(args);
+	if (args_number(args) > 2)
+	{
+		ft_putendl_fd("cd: too many arguments", 1);
+		return (ERROR);
+	}
+	if (chdir(args[1]) == -1)
+	{
+		ft_printf("%s : ", args[0]);
+		ft_putendl_fd(strerror(errno), 1);
+		return (ERROR);
+	}
+	return (SUCCESS);
 }
